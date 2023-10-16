663dcbd53fed30d5cdd101c122f216b0be9cbf13
spowers1991 <stevenpowers1991@gmail.com>
2023-10-16 23:04:23 +0100

    fix padding issues by adding a new breakpoint sxl

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Footer = () => {
   return (
-      <footer className='mx-auto container font-inter px-6 xl:px-3'>
+      <footer className='mx-auto container font-inter px-6 sxl:px-3'>
         <div className="lg:flex lg:justify-between lg:items-center py-6">
             <div className="text-center mx-auto">
                 © Steven Powers 2023 
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -18,7 +18,8 @@ const config: Config = {
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
       screens: {
-        'xxl': '2500px',
+        'sxl': '1500px',
+        'xxl': '2500px'
       },
     },
   },
