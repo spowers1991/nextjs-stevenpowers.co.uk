4c249bc8bc4323fba19023989dd5fd9d67d04412
spowers1991 <stevenpowers1991@gmail.com>
2023-10-16 16:42:21 +0100

    misc fixes 16

--- next.config.js
+++ next.config.js
@@ -3,15 +3,6 @@ const nextConfig = {
     images: {
       domains: ['a.storyblok.com'], // Add 'a.storyblok.com' to the list of allowed domains
     },
-    async redirects() {
-      return [
-        {
-          source: '/:path*',
-          destination: '/',
-          permanent: false,
-        },
-      ];
-    },
   };
   
   module.exports = nextConfig;
