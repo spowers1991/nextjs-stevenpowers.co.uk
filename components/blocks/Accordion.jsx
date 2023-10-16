062c3962fd071c8c4f1bfbeb530fecc7660c635d
spowers1991 <stevenpowers1991@gmail.com>
2023-10-17 00:58:13 +0100

    more padding changes 2

--- components/blocks/Accordion.jsx
+++ components/blocks/Accordion.jsx
@@ -19,7 +19,7 @@ const Accordion = ({ blok }) => {
   });
 
   return (
-    <div ref={ref} className={`container mx-auto px-6 xl:px-12 3xl:px-6 grid grid-cols-1 md:grid-cols-2 gap-14 md:gap-20 py-16 md:py-24 border-b duration-500 ${inView ? 'opacity-100' : 'opacity-20'}`} {...storyblokEditable(blok)}>
+    <div ref={ref} className={`container mx-auto px-6 xl:px-16 3xl:px-6 grid grid-cols-1 md:grid-cols-2 gap-14 md:gap-20 py-16 md:py-24 border-b duration-500 ${inView ? 'opacity-100' : 'opacity-20'}`} {...storyblokEditable(blok)}>
       <div>
         <Heading size="h3" className={`text-3xl sm:text-4xl md:text-5xl font-bold z-20 order-first ${blok.row_reverse ? 'md:order-last md:text-right' : 'md:order-first md:text-left'} text-left `}>
             {blok.title}
