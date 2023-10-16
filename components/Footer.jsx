7ff3b7850a386bdbc1a0e706267e1b964339b69c
spowers1991 <stevenpowers1991@gmail.com>
2023-10-17 00:59:46 +0100

    more padding changes, fix classes

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Footer = () => {
   return (
-      <footer className='mx-auto container font-inter px-6 xl:px-16 3xl:6'>
+      <footer className='mx-auto container font-inter px-6 xl:px-16 3xl:px-6'>
         <div className="lg:flex lg:justify-between lg:items-center py-6">
             <div className="text-center mx-auto">
                 © Steven Powers 2023 
