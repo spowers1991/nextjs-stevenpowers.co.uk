2ed82fcd512417feac1c2ce91292fc5ce004b46f
spowers1991 <stevenpowers1991@gmail.com>
2023-10-16 11:33:46 +0100

    test

--- components/blocks/ContactForm.jsx
+++ components/blocks/ContactForm.jsx
@@ -11,12 +11,12 @@ const ContactForm = () => {
             <Heading size="h3" className="pb-5">
                Send an email...
             </Heading>
-            <form className='w-full mx-auto sm:mr-16 sm:my-auto' name="contact" method="POST" onSubmit={handleFormSubmit} data-netlify={true} data-netlify-honeypot="bot-field">
+            <form className='w-full mx-auto sm:mr-16 sm:my-auto' name="contact" method="POST" onSubmit={handleFormSubmit}>
                 <input type="hidden" name="form-name" value="contact"  />
                 <input className="bg-[#fff]  rounded block w-full my-8 p-2  border-2 focus:border-[#434bed] hover:border-[#434bed] border-solid focus:border-solid placeholder-shown:border-[#434bed] border-[#434bed] placeholder-shown:border-dashed focus:outline-none focus:placeholder:text-black" type="text" id="name" name="name" placeholder='Name' required />
                 <input className="bg-[#fff]  rounded block w-full my-8 p-2  border-2 focus:border-[#434bed] hover:border-[#434bed] border-solid focus:border-solid placeholder-shown:border-[#434bed] border-[#434bed]  placeholder-shown:border-dashed focus:outline-none focus:placeholder:text-black" type="text" id="email" name="email" placeholder='Email' required />
                 <textarea rows="15" className=" bg-[#fff] rounded block w-full my-8 p-2  focus:border-[#434bed] border-2 hover:border-[#434bed] border-solid placeholder-shown:border-[#434bed] border-[#434bed]  focus:border-solid placeholder-shown:border-dashed focus:outline-none focus:placeholder:text-black" type="textarea" id="message" name="message" placeholder='Message' required/>
-                <button className={`submit-button ${submissionSuccess ? 'bg-black pointer-events-none' : 'bg-[#434bed]'} ${submissionFailure ? 'bg-red pointer-events-none' : 'bg-[#434bed]'} hover:bg-black duration-150 py-3 px-5 text-white rounded-full uppercase text-[11px] sm:text-xs font-[500] tracking-[1px] text-center" type="submit`}>
+                <button className={`submit-button ${submissionSuccess ? 'bg-black pointer-events-none' : 'bg-[#434bed]'} ${submissionFailure ? 'bg-[red] pointer-events-none' : 'bg-[#434bed]'} hover:bg-black duration-150 py-3 px-5 text-white rounded-full uppercase text-[11px] sm:text-xs font-[500] tracking-[1px] text-center" type="submit`}>
                     {!submissionSuccess && !submissionFailure ?
                         'Send message' 
                     :
