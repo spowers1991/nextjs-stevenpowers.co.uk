6ac5a6174107b9051552ecd164ccf59a7a58c4b5
spowers1991 <stevenpowers1991@gmail.com>
2023-10-16 12:53:01 +0100

    test5

--- pages/api/submitForm.js
+++ pages/api/submitForm.js
@@ -4,8 +4,8 @@ export default async function handler(req, res) {
   if (req.method === 'POST') {
     try {
       const data = req.body;
-      
-      
+      console.log(data)
+
     } catch (error) {
       console.error('Internal Server Error:', error);
       res.status(500).json({ message: 'Internal Server Error' });
