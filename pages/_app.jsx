0f2c612b88a6202fefce8e072304d2dc18be3587
spowers1991 <stevenpowers1991@gmail.com>
2023-10-20 15:45:35 +0100

    Add a Head component in _app.js

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,4 +1,5 @@
 import { storyblokInit, apiPlugin } from "@storyblok/react";
+import Head from 'next/head';
 
 // A story that we set up for setting Global site data. e.g site tite and menu data.
 import GetStory from '@/utils/GetStory'
@@ -43,10 +44,11 @@ storyblokInit({
 function MyApp({ Component, pageProps, globalSettings }) {
   return (
     <>
-      <link rel="preconnect" href="https://fonts.googleapis.com" />
-      <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
-      <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400&display=swap" rel="stylesheet" async />
-      
+      <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
+        <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400&display=swap" rel="stylesheet" async />
+      </Head>
       <Header globalSettings={globalSettings}/>
         <Component {...pageProps} />
       <Footer />
