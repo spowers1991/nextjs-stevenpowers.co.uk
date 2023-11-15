7e62f84e5f33c93c3a859c0a0d9a2f62cb05760f
spowers1991 <stevenpowers1991@gmail.com>
2023-11-15 16:34:04 +0000

    feat: add the slider arrows to the statis images slider

--- components/blocks/StaticImagesSlider.jsx
+++ components/blocks/StaticImagesSlider.jsx
@@ -22,6 +22,16 @@ const [ref, inView] = useInView({
   return (
     <>
       <div ref={ref} className={`relative duration-1000 ease-out ${inView ? 'opacity-100' : 'opacity-20'}`}  {...storyblokEditable(blok)}>
+      <div className='container mx-auto flex my-6 px-6 xl:px-16 3xl:px-0 pt-8 lg:pt-0'>
+            <div className='hidden md:flex gap-x-3 text-[#fff] ml-auto mt-6 sm:mt-0'>
+              <div className={`flex items-center bg-[#434bed] w-10 h-10 sm:w-12 sm:h-12   swiper-prev-${blok._uid} cursor-pointer duration-150`}>
+                  <svg className="w-4 h-4 sm:w-5 sm:h-5 mx-auto" fill="currentColor" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M0 12c0 6.627 5.373 12 12 12s12-5.373 12-12-5.373-12-12-12-12 5.373-12 12zm7.58 0l5.988-5.995 1.414 1.416-4.574 4.579 4.574 4.59-1.414 1.416-5.988-6.006z"/></svg>
+              </div>
+              <div className={`hidden md:flex items-center bg-[#434bed] w-10 h-10 sm:w-12 sm:h-12  swiper-next-${blok._uid} cursor-pointer  duration-150`} >
+                  <svg className="w-4 h-4 sm:w-5 sm:h-5 mx-auto" fill="currentColor" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M12 0c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm-1.568 18.005l-1.414-1.415 4.574-4.59-4.574-4.579 1.414-1.416 5.988 5.995-5.988 6.005z"/></svg>
+              </div>
+            </div>
+        </div>
         <Swiper
           slidesPerView={1.5}
           pagination={{
@@ -38,7 +48,7 @@ const [ref, inView] = useInView({
           resistanceRatio={0}
           centeredSlides={false}
           spaceBetween={15}
-          className={`static-tmages-slider my-16 md:my-24 xxl:container`}
+          className={`static-images-slider xxl:container`}
         >
             {blok.images.map(( image, index ) => (
                 <SwiperSlide className='relative group pb-6' key={index}>
