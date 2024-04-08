3aba7e82932f2d90366a3516d8894f4bd36a4b84
spowers1991 <stevenpowers1991@gmail.com>
2024-04-08 12:47:47 +0100

    chore(SwiperSlider): change padding values.

--- components/blocks/StaticImagesSlider.jsx
+++ components/blocks/StaticImagesSlider.jsx
@@ -21,7 +21,17 @@ const [ref, inView] = useInView({
 
   return (
     <>
-      <div ref={ref} className={`sm:-mt-24 relative duration-1000 ease-out ${inView ? 'opacity-100' : 'opacity-20'}`}  {...storyblokEditable(blok)}>
+      <div ref={ref} className={`-mt-12 sm:!-mt-16 relative duration-1000 ease-out ${inView ? 'opacity-100' : 'opacity-20'}`}  {...storyblokEditable(blok)}>
+      <div className='container mx-auto flex my-6 px-6 xl:px-16 3xl:px-0 pt-8 lg:pt-0'>
+            <div className='hidden md:flex gap-x-3 text-[#fff] ml-auto mt-6 sm:mt-0'>
+              <div className={`flex items-center bg-[#434bed] py-[11px] sm:py-[12px] px-5 swiper-prev-${blok._uid} cursor-pointer duration-150`}>
+                  <svg className="w-4 h-4 sm:w-5 sm:h-5 mx-auto" fill="currentColor" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M0 12c0 6.627 5.373 12 12 12s12-5.373 12-12-5.373-12-12-12-12 5.373-12 12zm7.58 0l5.988-5.995 1.414 1.416-4.574 4.579 4.574 4.59-1.414 1.416-5.988-6.006z"/></svg>
+              </div>
+              <div className={`hidden md:flex items-center bg-[#434bed] py-[11px] sm:py-[12px] px-5  swiper-next-${blok._uid} cursor-pointer  duration-150`} >
+                  <svg className="w-4 h-4 sm:w-5 sm:h-5 mx-auto" fill="currentColor" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M12 0c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm-1.568 18.005l-1.414-1.415 4.574-4.59-4.574-4.579 1.414-1.416 5.988 5.995-5.988 6.005z"/></svg>
+              </div>
+            </div>
+        </div>
         <Swiper
           slidesPerView={1.5}
           pagination={{
