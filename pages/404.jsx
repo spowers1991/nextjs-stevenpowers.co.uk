e3e8b8fe0d3496350f61da3683d5ec26b32fcd76
spowers1991 <stevenpowers1991@gmail.com>
2024-08-10 16:34:58 +0100

    fix(404): added main to fix layout

--- pages/404.jsx
+++ pages/404.jsx
@@ -5,17 +5,19 @@ const NotFound = () => {
 
     return(
       <Meta>
-          <div className={`text-center font-inter mx-auto container my-16 md:my-24  px-6 xl:px-0 mt-[50px] duration-500 `} >  
+          <main className={`font-inter bg-[#f4f4f4] overflow-hidden pt-20`}>
+            <div className={`text-center font-inter mx-auto container my-16 md:my-24  px-6 xl:px-0 mt-[50px] duration-500 `} >  
 
-              <h2 className="text-3xl sm:text-4xl md:text-5xl font-bold z-20 mb-12 md:mb-20 leading-[1.1] md:leading-[1]">
-                  <span className="text-[#434bed] underline">404</span> - Page Not Found
-              </h2>
+                <h2 className="!text-black text-3xl sm:text-4xl md:text-5xl font-bold z-20 mb-12 md:mb-20 leading-[1.1] md:leading-[1]">
+                    <span className="text-[#434bed] underline">404</span> - Page Not Found
+                </h2>
 
-              <Link href="/"className="relative bg-[#434bed] hover:bg-black duration-150 py-3 px-5 text-white rounded-full uppercase text-[11px] sm:text-xs font-[500] tracking-[1px]">
-                      Back to the homepage
-              </Link>
-            
-          </div>
+                <Link href="/"className="relative bg-[#434bed] hover:bg-black duration-150 py-3 px-5 text-white rounded-full uppercase text-[11px] sm:text-xs font-[500] tracking-[1px]">
+                        Back to the homepage
+                </Link>
+                
+            </div>
+          </main>
         </Meta>
     )
 };
