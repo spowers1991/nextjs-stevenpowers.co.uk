3ff077666cf34a05289e9bc6a95ffb1152b70281
spowers1991 <stevenpowers1991@gmail.com>
2024-08-25 22:46:53 +0100

    debug: revalidate

--- pages/api/revalidate.js
+++ pages/api/revalidate.js
@@ -1,18 +1,18 @@
 // pages/api/revalidate.js
 export default async function handler(req, res) {
-    if (req.method !== 'POST') {
-      return res.status(405).json({ message: 'Only POST requests allowed' });
-    }
-  
-    const { path } = req.body;
-    console.log(path)
-    try {
-      // Trigger revalidation
-      await res.revalidate(path);
-      return res.status(200).json({ message: `Revalidated ${path}` });
-    } catch (err) {
-      console.error('Error revalidating:', err);
-      return res.status(500).json({ message: 'Error revalidating' });
-    }
+  if (req.method !== 'POST') {
+    return res.status(405).json({ message: 'Only POST requests allowed' });
   }
-  
+
+  const { path } = req.body;
+  console.log(path);
+
+  try {
+    // Set the Revalidate header to trigger revalidation
+    res.setHeader('Revalidate', '1');
+    return res.status(200).json({ message: `Revalidated ${path}` });
+  } catch (err) {
+    console.error('Error revalidating:', err);
+    return res.status(500).json({ message: 'Error revalidating' });
+  }
+}
