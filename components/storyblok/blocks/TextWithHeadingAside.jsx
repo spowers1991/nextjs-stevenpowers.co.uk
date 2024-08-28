6804e3e147edf7eed5fd46c3b96ed8b53894fdda
spowers1991 <stevenpowers1991@gmail.com>
2024-08-28 21:40:25 +0100

    fix( <TextWithHeadingAside /> ): add missing key value to map props

--- components/storyblok/blocks/TextWithHeadingAside.jsx
+++ components/storyblok/blocks/TextWithHeadingAside.jsx
@@ -13,7 +13,7 @@ const TextWithHeadingAside = ({ blok }) => {
 
     return(
         <div ref={ref} className={`container mx-auto px-6 md:px-12 xl:px-16 3xl:px-0 my-12 md:my-24 duration-500 ${inView ? 'opacity-100' : 'opacity-20'}`} {...storyblokEditable(blok)}>
-            <div class={`items-center grid grid-cols-1 md:grid-cols-2 gap-8 md:gap-20  ${blok.border && 'pb-10 md:pb-24 border-b'}`}>
+            <div className={`items-center grid grid-cols-1 md:grid-cols-2 gap-8 md:gap-20  ${blok.border && 'pb-10 md:pb-24 border-b'}`}>
                 <Heading size="h3" className={`text-3xl sm:text-4xl md:text-5xl font-bold z-20 order-first ${blok.row_reverse ? 'md:order-last md:text-right' : 'md:order-first md:text-left'} text-left `}>
                     <RichText content={blok.title} />    
                 </Heading>
