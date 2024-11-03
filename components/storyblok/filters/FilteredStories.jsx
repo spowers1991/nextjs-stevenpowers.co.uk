7121b9a089448727b576d3187b61c99b5f2d1809
Steven Powers <stevenpowers1991@gmail.com>
2024-11-03 20:19:30 +0000

    refactor: better breakpoints on projects

--- components/storyblok/filters/FilteredStories.jsx
+++ components/storyblok/filters/FilteredStories.jsx
@@ -7,7 +7,7 @@ const FilteredStories = ({ stories, page }) => {
   return (
     <>
         {stories?.length > 0 ?
-            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-x-6 gap-y-12">
+            <div className="grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-3 gap-x-6 gap-y-12">
             {stories?.slice((page - 1) * 6, page * 6).map((story, index) => (
                 <Card key={story.uuid} index={index} story={story} />
             ))}
